03e3d1b5894508fb93a176ba32ca1b5493df8f03
jigna sherasiya <Jigna.sherasiya@sonos.tech>
2022-01-15 11:14:35 +0530

    replace string from input file

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
-from json import load
+from json import loads
 import re
 
 with open("test.json") as json_file:
-    data = load(json_file)
-# print(data)s
-# data = loads(data)
-for key in data:
-    print(type(key))
-    new_key = re.sub("+test","+size@5+type",key)
-    # new_key = key.replace("+test","+size@5+type")
-    print(new_key)
+    data = loads(json_file.read())
+
+output_data = {}
+for key,values in data.items():
+    new_key = key.replace("+type","+size@5+type")
+    output_data[str(new_key)] = {}
+    for val in values:
+        item = data[key][val]
+        if type(item) == str:
+            new_value = item.replace("+type","+size@5+type")
+            output_data[new_key][val] = new_value
+            # print(new_value)
+        else:
+            new_value_list = []
+            for x in item:
+                new_value = x.replace("+type","+size@5+type")
+                new_value_list.append(new_value)
+                # print(new_value)
+            output_data[new_key][val] = new_value_list
+
+print(output_data)

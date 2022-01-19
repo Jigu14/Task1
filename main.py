5e93d3956fe4a449aa973a003f2deadccb1354f8
jigna sherasiya <Jigna.sherasiya@sonos.tech>
2022-01-19 21:04:50 +0530

    update recursion function

--- main.py
+++ main.py
@@ -4,44 +4,54 @@ import logging
 logging.basicConfig(filename='./app.log', filemode='w',level=logging.DEBUG, format='%(asctime)s | %(name)s | %(levelname)s | %(message)s')
 logger = logging.getLogger()
 
-try:
-    # open input file
-    with open("./test.json") as json_file:
-        data = loads(json_file.read())
-        logger.info("Open test.json file.")
+def recursion_fun(data):
+    try:
+        # replace string function
+        def replace_fun(name):
+            s1 = name.replace("+type","+size@5+type")
+            return s1
 
-    # empty outpput dict
-    output_data = {}
-    for key,values in data.items():
-        # replace +type work from key
-        new_key = key.replace("+type","+size@5+type")
-        logger.debug("old key: {} and new_key : {}".format(key, new_key))
-        # new_key update on output_data
-        output_data[new_key] = {}
-        for val in values:
-            item = data[key][val]
-            # check type of item
-            if type(item) == str:
-                new_value = item.replace("+type","+size@5+type")
-                output_data[new_key][val] = new_value
-                logger.debug("old value: {} and new_value : {}".format(val, new_value))
-            else:
-                new_value_list = []
-                for x in item:
-                    new_value = x.replace("+type","+size@5+type")
-                    new_value_list.append(new_value)
-                    logger.debug("old value: {} and new_value : {}".format(val, new_value))
-                output_data[new_key][val] = new_value_list
+        # object type is dict
+        if isinstance(data, dict):
+            # empty outpput dict
+            output_data = {}
+            for key,value in data.items():
+                # call replace function
+                new_key = replace_fun(key)
+                # check value type is string or dict
+                # if dict then call recursion function
+                # else call replace string function
+                if type(value) == str and "+type" in value:
+                    new_value = replace_fun(value)
+                    output_data[new_key] = new_value
+                else:
+                    value = recursion_fun(value)
+                    output_data[new_key] = value
+            return output_data
 
-    logger.debug("Result : {}".format(output_data))
-    # create output json file and write
-    with open("./output.json","w") as file:
-        file.write(dumps(output_data, indent=4))
-        logger.info("data updated in output.json file")
+        # object type is list
+        if isinstance(data, list):
+            new_list = []
+            for value in data:
+                new_list.append(replace_fun(value))
+            return new_list
 
-except KeyError as key_error:
-    logger.error({"Error":"The following key is not found {}".format(str(key_error))})
-except ValueError as value_error:
-    logger.error({"Error":"Value Error {}".format(str(value_error))})
-except Exception as e:
-    logger.error({"Error":"Exception {}".format(str(e))})
+        return data
+
+    except KeyError as key_error:
+        logger.error({"Error":"The following key is not found {}".format(str(key_error))})
+    except ValueError as value_error:
+        logger.error({"Error":"Value Error {}".format(str(value_error))})
+    except Exception as e:
+        logger.error({"Error":"Exception {}".format(str(e))})
+
+
+# open input file
+with open("./test.json") as json_file:
+    input_data = loads(json_file.read())
+    logger.info("Open test.json file.")
+
+# create output file
+with open("./output.json","w") as file:
+    file.write(dumps(recursion_fun(input_data), indent=4))
+    logger.info("data updated in output.json file")
